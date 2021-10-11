c4ab0ec302b77eb6c7a572ffe418f8e79d5f0cb0
MikeS <mikes@metsi.co.uk>
2021-10-11 10:32:52 +0100

    updated location for both locations

--- 07-Multiple-Provider-Configurations/terraform-manifests/c2-resource-group.tf
+++ 07-Multiple-Provider-Configurations/terraform-manifests/c2-resource-group.tf
@@ -1,13 +1,13 @@
 # Create a resource group in EastUS region - Uses Default Provider
 resource "azurerm_resource_group" "myrg1" {
   name = "myrg-1"
-  location = "East US"
+  location = "uksouth"
 }
 
 #Create a resource group in WestUS region - Uses "provider2-westus" provider
 resource "azurerm_resource_group" "myrg2" {
   name = "myrg-2"
-  location = "West US"
+  location = "ukwest"
   provider = azurerm.provider2-westus
 }
 
